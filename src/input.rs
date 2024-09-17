d96089f0bef7a0a4df94ad836da5a1d045ab5e6d
coding398 <55938439+codingMASTER398@users.noreply.github.com>
2024-09-17 18:43:02 +1000

    what am i doing

--- src/input.rs
+++ src/input.rs
@@ -208,4 +208,84 @@ impl<Manager> Input<Manager> {
             sys::SteamAPI_ISteamInput_Shutdown(self.input);
         }
     }
+
+    pub fn get_action_glyph_path(
+        &self,
+        input_handle: sys::InputHandle_t,
+        action_set_handle: sys::InputActionSetHandle_t,
+        action_name: &str,
+    ) -> Option<String> {
+        // Retrieve the digital or analog action handle based on the action name.
+        let digital_action_handle = self.get_digital_action_handle(action_name);
+        let analog_action_handle = self.get_analog_action_handle(action_name);
+    
+        let origins = if let Some(digital_handle) = digital_action_handle {
+            // Get digital action origins
+            self.get_digital_action_origins(input_handle, action_set_handle, digital_handle)
+        } else if let Some(analog_handle) = analog_action_handle {
+            // Get analog action origins
+            self.get_analog_action_origins(input_handle, action_set_handle, analog_handle)
+        } else {
+            // No matching action found
+            return None;
+        };
+    
+        // Convert origins to glyph paths if any origins exist
+        if let Some(origins_vec) = origins {
+            // For simplicity, we'll return the first origin's glyph path
+            if let Some(first_origin) = origins_vec.first() {
+                return self.get_glyph_for_action_origin(*first_origin);
+            }
+        }
+    
+        None
+    }
+
+    pub fn get_analog_action_origins(
+        &self,
+        input_handle: sys::InputHandle_t,
+        action_set_handle: sys::InputActionSetHandle_t,
+        analog_action_handle: sys::InputAnalogActionHandle_t,
+    ) -> Option<Vec<EInputActionOrigin>> {
+        let mut origins_out = [0 as EInputActionOrigin; STEAM_INPUT_MAX_ORIGINS as usize];
+        let num_origins = unsafe {
+            sys::SteamAPI_ISteamInput_GetAnalogActionOrigins(
+                self.input,
+                input_handle,
+                action_set_handle,
+                analog_action_handle,
+                origins_out.as_mut_ptr(),
+            )
+        };
+    
+        if num_origins > 0 {
+            Some(origins_out[..num_origins as usize].to_vec())
+        } else {
+            None
+        }
+    }
+
+    pub fn get_digital_action_origins(
+        &self,
+        input_handle: sys::InputHandle_t,
+        action_set_handle: sys::InputActionSetHandle_t,
+        digital_action_handle: sys::InputDigitalActionHandle_t,
+    ) -> Option<Vec<EInputActionOrigin>> {
+        let mut origins_out = [0 as EInputActionOrigin; STEAM_INPUT_MAX_ORIGINS as usize];
+        let num_origins = unsafe {
+            sys::SteamAPI_ISteamInput_GetDigitalActionOrigins(
+                self.input,
+                input_handle,
+                action_set_handle,
+                digital_action_handle,
+                origins_out.as_mut_ptr(),
+            )
+        };
+    
+        if num_origins > 0 {
+            Some(origins_out[..num_origins as usize].to_vec())
+        } else {
+            None
+        }
+    }
 }
